a22ae4ca79f155b8a4205611f255389bf5de2e74
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-08-19 20:31:31 -0300

    create(post), update(put), delete(splice)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 const jogos = [
     'Mobile Legends: Bang Bang',
     'Need for Speed Underground 2',
@@ -20,7 +22,7 @@ const jogos = [
 ];
 
 app.get('/',(req,res)=>{
-    res.send('Bem vindos a minha lista de jogos. Acesse: http://localhost:3000/jogos para ver a lista')
+    res.send('Bem vindos a minha lista de jogos.')
 });
 
 app.get('/jogos',(req,res)=>{
@@ -28,20 +30,49 @@ app.get('/jogos',(req,res)=>{
 });
 
 app.get('/jogos/:id',(req, res) => {
-    const id = req.params.id;
+    const id = req.params.id -1;
     const game = jogos[id] ;
-    if (id > jogos.length -1 || id < 0){
-        res.send('Id inválido! Tente Novamente');
-    } else{
-        res.send(game)
+    if (!game) {
+        res.send('Id Inválido. Jogo Não Encontrado!');
     }
+    res.send(game);
     
 });
 
 app.get('/jogo-aleatorio', (req, res)=>{
     let jogoRandom = jogos[Math.floor(Math.random()*jogos.length)];
     res.send(jogoRandom);
-})
+});
+
+
+// Creat (Post)
+app.post('/jogos', (req,res) => {
+    const jogo = req.body.jogo;
+    jogos.push(jogo);
+
+    res.send(`O jogo ${jogo} foi adicionado com sucesso!`)
+});
+
+
+// Update (put)
+app.put('/jogos/:id', (req,res) => {
+    const id = req.params.id -1;
+    const jogoAntigo =  jogos[id];
+    const jogo = req.body.jogo;
+    jogos[id] = jogo;
+
+    res.send(`O jogo ${jogoAntigo} foi atualizado para ${jogo} com sucesso`)
+});
+
+//Delete com splice para não ficar o null na lista e fazer os filmes subirem de id
+app.delete('/jogos/:id', (req,res)=>{
+    const id = req.params.id -1;
+    const jogoAexcluir = jogos[id];
+    jogos.splice(id,1);
+
+    res.send(`O jogo ${jogoAexcluir} foi excluído com sucesso.`);
+});
+
 
 
 app.listen(port,()=>{
